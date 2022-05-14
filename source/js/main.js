a4b43390450f246edaf2f75e028047ac2179f515
Артем Быков <37114203+nisven@users.noreply.github.com>
2022-05-14 17:28:25 +0300

    Подвал проекта

--- source/js/main.js
+++ source/js/main.js
@@ -20,6 +20,13 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+document.querySelectorAll('[data-accordion]').forEach((accordion) => {
+  accordion.addEventListener('click', ()=> {
+    accordion.closest('.footer__section').classList.toggle('is--close');
+    accordion.closest('.footer__section').classList.toggle('is--open');
+  });
+});
+
 // ---------------------------------
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
