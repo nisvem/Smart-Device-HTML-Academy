89294435b3ecca6f3a07bfa4c1737f31015493d3
Артем Быков <37114203+nisven@users.noreply.github.com>
2022-05-19 09:40:01 +0300

    Поправил footer

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,6 +1,18 @@
-document.querySelectorAll('[data-accordion]').forEach((accordion) => {
+let accordions = document.querySelectorAll('[data-accordion]');
+
+accordions.forEach((accordion) => {
+  accordions.forEach((ac) => {
+    ac.closest('.footer__section').classList.remove('footer__section--open');
+  });
+
   accordion.addEventListener('click', ()=> {
-    accordion.closest('.footer__section').classList.toggle('footer__section--close');
-    accordion.closest('.footer__section').classList.toggle('footer__section--open');
+    if (accordion.closest('.footer__section').classList.contains('footer__section--open')) {
+      accordion.closest('.footer__section').classList.remove('footer__section--open');
+    } else {
+      accordions.forEach((ac) => {
+        ac.closest('.footer__section').classList.remove('footer__section--open');
+      });
+      accordion.closest('.footer__section').classList.add('footer__section--open');
+    }
   });
 });
